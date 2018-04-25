0b8473181c4bf10c5c6860fd8c165d64382647bd
ShiraoDainichi <petitromainarras@gmail.com>
2018-04-25 12:00:30 +0200

    ajout soustraction

--- SommeDeDeuxNombre/src/Main.java
+++ SommeDeDeuxNombre/src/Main.java
@@ -5,13 +5,19 @@ public class Main {
 		return a+b;
 	}
 	
+	public static int soustractionDeDeuxNombr(int a, int b) {
+		return a-b;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int premier = 15, deuxieme = 25;
 		int troisieme = sommeDeDeuxNombre(premier,deuxieme);
+		int quatrieme = soustractionDeDeuxNombr(troisieme,deuxieme);
 		System.out.println("premier : "+premier);
 		System.out.println("deuxieme : "+deuxieme);
-		System.out.println("troisieme : "+troisieme);
+		System.out.println("troisieme (la somme de deux nombre : premier + deuxieme) : "+troisieme);
+		System.out.println("quatrieme (la soustraction de deux nombre : troisieme - deuxieme) : "+quatrieme);
 	}
 
 }

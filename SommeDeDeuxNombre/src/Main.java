8c523be37ce2da004bbc32a1df11edc41e6cd858
ShiraoDainichi <petitromainarras@gmail.com>
2018-04-25 11:47:13 +0200

    third commit

---
+++ SommeDeDeuxNombre/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static int sommeDeDeuxNombre(int a, int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int premier = 15, deuxieme = 25;
+		int troisieme = sommeDeDeuxNombre(premier,deuxieme);
+		System.out.println("premier : "+premier);
+		System.out.println("deuxieme : "+deuxieme);
+		System.out.println("troisieme : "+troisieme);
+	}
+
+}

50b411db9f89d5ce59ee6c972397cd30fa8b0196
ShiraoDainichi <petitromainarras@gmail.com>
2018-04-25 12:13:18 +0200

    ajout multiplication

--- SommeDeDeuxNombre/src/Main.java
+++ SommeDeDeuxNombre/src/Main.java
@@ -5,13 +5,18 @@ public class Main {
 		return a+b;
 	}
 	
+	public static int multiplicationDeDeuxNombre(int a, int b) {
+		return a*b;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int premier = 15, deuxieme = 25;
 		int troisieme = sommeDeDeuxNombre(premier,deuxieme);
+		int cinquieme = multiplicationDeDeuxNombre(premier, deuxieme);
 		System.out.println("premier : "+premier);
 		System.out.println("deuxieme : "+deuxieme);
-		System.out.println("troisieme : "+troisieme);
+		System.out.println("cinquieme (multiplication de deux nombre : premier x deuxieme ) : "+cinquieme);
 	}
 
 }
